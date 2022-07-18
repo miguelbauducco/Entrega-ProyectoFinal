d5945fd7e24511414fa4c2664602ff486afd96d2
miguelbauducco <miguel.bau14@gmail.com>
2022-07-18 15:52:16 -0300

    proyecto actualizado 18/7

--- js/form2.js
+++ js/form2.js
@@ -4,6 +4,6 @@
 let divCliente = document.getElementById("cliente");
 let h2Element = document.createElement("h2");
 let nombreCartel = sessionStorage.getItem('nombre');
-h2Element.innerText = "Gracias por tu consulta, "+ console.dir(nombreCartel);
+h2Element.innerText = "Gracias por tu consulta, "+ console.log(nombreCartel);
 
 divCliente.append(h2Element);
