98d251540941c6ab95ee9c95704a37ae183e5566
miguelbauducco <miguel.bau14@gmail.com>
2022-08-02 13:41:45 -0300

    fetch funcionando

--- js/comments.js
+++ js/comments.js
@@ -1,24 +1,59 @@
 //UTILIZACION DE FETCH
 
-const clienteName = document.getElementById("clientename");
+const clienteName1 = document.getElementById("clientename1");
+const clienteName2 = document.getElementById("clientename2");
+const clienteName3 = document.getElementById("clientename3");
 
-const h5Element = document.createElement("h5");
 
-let nombre;
+const h5Element1 = document.createElement("h5");
+const h5Element2 = document.createElement("h5");
+const h5Element3 = document.createElement("h5");
 
-function traerCliente(){
+
+function traerCliente1(){
 
     fetch("https://rickandmortyapi.com/api/character/2")
     .then ( (response) =>
     response.json()
     )
-    .then ((json) => console.log(json.name));
-
-    h5Element.innerHTML = JSON.name;
+    .then ((json) => h5Element1.innerHTML = (json.name));
 
 }
 
-traerCliente();
 
 
-clienteName.append(h5Element);
+function traerCliente2(){
+
+    fetch("https://rickandmortyapi.com/api/character/168")
+    .then ( (response) =>
+    response.json()
+    )
+    .then ((json) => h5Element2.innerHTML = (json.name));
+
+}
+
+
+
+function traerCliente3(){
+
+    fetch("https://rickandmortyapi.com/api/character/172")
+    .then ( (response) =>
+    response.json()
+    )
+    .then ((json) => h5Element3.innerHTML = (json.name));
+
+}
+
+
+traerCliente1();
+traerCliente2();
+traerCliente3();
+
+
+
+
+clienteName1.append(h5Element1);
+clienteName2.append(h5Element2);
+clienteName3.append(h5Element3);
+
+
